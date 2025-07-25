8ceff71e4aa4f060233f5f71e588bd6772a24982
KustelR <dmitriysstudios2@gmail.com>
2025-07-25 15:27:55 +0400

    chore: moving footer and header to layout as they persist across every page

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,3 +1,6 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -7,21 +10,22 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Conduit</title>
-  <link
-    href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
-    rel="stylesheet"
-    type="text/css"
-  />
-  <link
-    href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
-    rel="stylesheet"
-    type="text/css"
-  />
-  <link rel="stylesheet" href="//demo.productionready.io/main.css" />
+        <link
+          href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link
+          href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link rel="stylesheet" href="//demo.productionready.io/main.css" />
       </head>
-      <body
-      >
+      <body>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
