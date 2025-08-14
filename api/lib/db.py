6dacd57d0a1f739f828968ea975b2478b2bcd1f4
KustelR <dmitriysstudios2@gmail.com>
2025-08-14 15:56:43 +0400

    feat: implemented article deletion

--- api/lib/db.py
+++ api/lib/db.py
@@ -1,10 +1,11 @@
 import os
 import pymongo
 
-from schemas import Article
+from schemas import Article, DatabaseArticle
 
 
 client = pymongo.MongoClient(os.environ.get("MONGODB_CONNECTION"))
+exclude = {"_id": 0, "deleted": 0}
 db = client["realworld"]
 articlesCollection = db["articles"]
 
@@ -12,15 +13,16 @@ print("Connected to MongoDB")
 
 
 def readArticles():
-    return list(articlesCollection.find({}, {"_id": 0}))
+    return list(articlesCollection.find({"deleted": False}, exclude))
 
 
 def readArticle(slug: str):
-    return articlesCollection.find_one({"slug": slug}, {"_id": 0})
+    return articlesCollection.find_one({"deleted": False, "slug": slug}, exclude)
 
 
 def createArticle(article: Article):
-    articlesCollection.insert_one(article.model_dump())
+
+    articlesCollection.insert_one(DatabaseArticle.model_validate(article.model_dump()).model_dump())
     return article
 
 
@@ -28,3 +30,7 @@ def updateArticle(slug: str, article: Article):
     new = {k: v for k,v in article.model_dump().items() if v is not None}
     articlesCollection.update_one({"slug": slug}, {"$set": new})
     return article
+
+
+def deleteArticle(slug: str):
+    articlesCollection.delete_many({"slug": slug})
