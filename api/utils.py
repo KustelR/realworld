84947b07da80749225e55e6043e2c252ae3831b4
KustelR <dmitriysstudios2@gmail.com>
2025-08-15 15:02:43 +0400

    feat: function for auth token extraction

--- api/utils.py
+++ api/utils.py
@@ -1,3 +1,13 @@
+from fastapi import Request, HTTPException
+
+
 
 def getSlug(title: str) -> str:
     return title.lower().replace(" ", "-")
+
+
+def getTokenFromRequest(req: Request) -> str:
+    try:
+        return req.headers.get("Authorization").split(" ")[1]
+    except Exception as e:
+        raise HTTPException(401, "Unprocessable authentification header")
