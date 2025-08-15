c89d80d70fccdd8529f5fb6b7b2d1aec90ca46bd
KustelR <dmitriysstudios2@gmail.com>
2025-08-15 15:50:32 +0400

    feat: profiles endpoint

---
+++ api/routers/profiles.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, HTTPException, Request
+
+from lib.auth import getEmailFromToken
+from schemas import User, UserDatabase
+from utils import authentificateRequest, getTokenFromRequest
+from lib.db import followUser, getProfile, unfollowUser
+from lib.db import getUser, isFollowing 
+
+
+router = APIRouter()
+
+
+@router.get("/{username}")
+async def get_profile(username: str, req: Request):
+    user: UserDatabase
+    isAuthenticated = True
+    try:
+        user = authentificateRequest(req)
+    except Exception as e:
+        isAuthenticated = False
+
+    profile = getProfile(username)
+    if not profile:
+        raise HTTPException(404, "Profile not found")
+    
+    if isAuthenticated:
+        profile["following"] = isFollowing(user.username, username)
+    
+    return {"profile": profile}
+
+
+@router.post("/{username}/follow")
+async def follow_user(username: str, req: Request):
+
+    user: UserDatabase
+    try:
+        user = authentificateRequest(req)
+    except Exception as e:
+        raise HTTPException(401, f"Authentication failed: {str(e)}")
+
+    if isFollowing(user.username, username):
+        raise HTTPException(400, "Already following this user")
+
+    profile = followUser(user.username, username)
+
+    return {"profile": profile}
+
+
+@router.delete("/{username}/follow")
+async def follow_user(username: str, req: Request):
+
+    user: UserDatabase
+    try:
+        user = authentificateRequest(req)
+    except Exception as e:
+        raise HTTPException(401, f"Authentication failed: {str(e)}")
+
+    if not isFollowing(user.username, username):
+        raise HTTPException(400, "Already not following this user")
+
+    profile = unfollowUser(user.username, username)
+
+    return {"profile": profile}
