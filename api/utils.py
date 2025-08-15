f98d400492e3e342ce0a2f922cdf8d39d9dc9bdb
KustelR <dmitriysstudios2@gmail.com>
2025-08-15 15:48:09 +0400

    feat: util function for checking request authentification

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,9 @@
 from fastapi import Request, HTTPException
 
+from schemas import User, UserDatabase
+from lib.auth import getEmailFromToken
+from lib.db import getUser
+
 
 
 def getSlug(title: str) -> str:
@@ -11,3 +15,22 @@ def getTokenFromRequest(req: Request) -> str:
         return req.headers.get("Authorization").split(" ")[1]
     except Exception as e:
         raise HTTPException(401, "Unprocessable authentification header")
+
+
+def authentificateRequest(req: Request) -> User:
+    token = getTokenFromRequest(req)
+    if not token:
+        raise Exception("Authorization header missing")
+
+    try:
+        email = getEmailFromToken(token)
+    except Exception as e:
+        raise Exception(f"Invalid token: {str(e)}")
+    
+    user = getUser(email)
+    if not user:
+        raise Exception("User not found")
+    
+    print(user)
+    
+    return UserDatabase.model_validate(user)
