a2d281293ecc9da04244165dd49578c8e5d2f2ca
KustelR <dmitriysstudios2@gmail.com>
2025-08-15 15:50:46 +0400

    feat: adding profiles endpoint

--- api/index.py
+++ api/index.py
@@ -3,10 +3,11 @@ load_dotenv()
 
 
 import fastapi
-from routers import articles, users, user
+from routers import articles, users, user, profiles
 
 
 app = fastapi.FastAPI()
 app.include_router(articles.router, prefix="/articles")
 app.include_router(users.router, prefix="/users")
 app.include_router(user.router, prefix="/user")
+app.include_router(profiles.router, prefix="/profiles")
