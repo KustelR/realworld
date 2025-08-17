5563b91969afd226031fa4ce519dcb969984ec57
KustelR <dmitriysstudios2@gmail.com>
2025-08-17 14:57:10 +0400

    feat: types for api responses

---
+++ web/types/index.ts
@@ -0,0 +1,18 @@
+interface Article {
+  slug: string;
+  title: string;
+  description: string;
+  body?: string;
+  tagList: string[];
+  createdAt: string;
+  updatedAt: string;
+  favoritesCount: number;
+  author: User;
+}
+
+interface User {
+  username: string;
+  bio: string;
+  image: string;
+  following?: boolean;
+}
