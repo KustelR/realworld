97bf34b7935583ada6a51b9a3f63cc0aa41ed65b
KustelR <dmitriysstudios2@gmail.com>
2025-08-18 18:02:55 +0400

    chore: allowed imgur in cors

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("http://i.imgur.com/**")],
+    remotePatterns: [
+      new URL("http://i.imgur.com/**"),
+      new URL("https://i.imgur.com/**"),
+    ],
   },
 };
 
