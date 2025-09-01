07c39526bd2281fdd04b4aef62ebc95989f805dc
KustelR <dmitriysstudios2@gmail.com>
2025-09-01 14:00:53 +0400

    fix: removed unused console log

--- web/src/app/register/page.tsx
+++ web/src/app/register/page.tsx
@@ -47,7 +47,6 @@ export default function Page() {
                     `'`,
                     `"`,
                   );
-                  console.log(errorMessage);
                   setErrors(JSON.parse(errorMessage));
                 }
               }}
