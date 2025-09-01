bc879fa6ec35e8845834826b9fa1b260e12339fe
KustelR <dmitriysstudios2@gmail.com>
2025-09-01 14:56:02 +0400

    feat: updating user profile

--- web/src/app/settings/page.tsx
+++ web/src/app/settings/page.tsx
@@ -1,15 +1,52 @@
+"use client";
+
 import ErrorMessages from "@/components/ErrorMessages";
 import LogoutButton from "@/components/LogoutButton";
+import fetchFromAPI from "@/lib/req/fetchClient";
+import { setCookie } from "cookies-next";
+import { useState } from "react";
 
 export default function Page() {
+  const [errors, setErrors] = useState<string[]>([]);
   return (
     <div className="settings-page">
       <div className="container page">
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Your Settings</h1>
-            <ErrorMessages messages={["That name is required"]} />
-            <UpdateForm />
+            <ErrorMessages messages={errors} />
+            <UpdateForm
+              onSubmit={async (username, image, bio, email, password) => {
+                const user = {
+                  username,
+                  image,
+                  bio,
+                  email,
+                  password,
+                };
+                const userPatch = Object.fromEntries(
+                  Object.entries(user).filter((entry) =>
+                    Boolean(entry ? entry[1] : false),
+                  ),
+                );
+                const response = await fetchFromAPI(`/user`, {
+                  method: "PUT",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify({ user: userPatch }),
+                });
+                if (response.ok) {
+                  const responseUser = (await response.json()).user;
+                  setCookie("Authorization", `Token ${responseUser.token}`);
+                  window.location.href = `/profile/${responseUser.username}`;
+                } else {
+                  const responseFail: FailedRequestBody = await response.json();
+                  const errors: string[] = JSON.parse(
+                    responseFail.detail.replaceAll("'", `"`),
+                  );
+                  setErrors(errors);
+                }
+              }}
+            />
             <hr />
             <LogoutButton />
           </div>
@@ -19,12 +56,35 @@ export default function Page() {
   );
 }
 
-function UpdateForm() {
+function UpdateForm(props: {
+  onSubmit: (
+    name: string,
+    image: string,
+    bio: string,
+    email: string,
+    password: string,
+  ) => void;
+}) {
+  const { onSubmit } = props;
+
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit(name, image, bio, email, password);
+      }}
+    >
       <fieldset>
         <fieldset className="form-group">
           <input
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
             className="form-control"
             type="text"
             placeholder="URL of profile picture"
@@ -32,6 +92,9 @@ function UpdateForm() {
         </fieldset>
         <fieldset className="form-group">
           <input
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
             className="form-control form-control-lg"
             type="text"
             placeholder="Your Name"
@@ -39,6 +102,9 @@ function UpdateForm() {
         </fieldset>
         <fieldset className="form-group">
           <textarea
+            onChange={(e) => {
+              setBio(e.target.value);
+            }}
             className="form-control form-control-lg"
             rows={8}
             placeholder="Short bio about you"
@@ -46,6 +112,9 @@ function UpdateForm() {
         </fieldset>
         <fieldset className="form-group">
           <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             autoComplete="off"
             className="form-control form-control-lg"
             type="text"
@@ -54,6 +123,9 @@ function UpdateForm() {
         </fieldset>
         <fieldset className="form-group">
           <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             autoComplete="new-password"
             className="form-control form-control-lg"
             type="password"
