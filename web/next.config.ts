61006caf6b563719440fbb50c05d77db02212088
KustelR <dmitriysstudios2@gmail.com>
2025-09-05 17:53:11 +0400

    chore: allowing all image sources

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,8 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      new URL("http://i.imgur.com/**"),
-      new URL("https://i.imgur.com/**"),
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
